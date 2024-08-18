d3b0fc8a219016e60071839c0ccd06aad7a8f56e
YasminTorresDeveloper <126827320+YasminTorresDeveloper@users.noreply.github.com>
2024-08-17 20:14:18 -0500

    Funciones de desencriptar,encriptar y copiar

---
+++ script.js
@@ -0,0 +1,62 @@
+const textArea= document.querySelector('.text-area');
+const mensaje= document.querySelector('.mensaje');
+
+/* 
+Las "llaves" de encriptación que utilizaremos son las siguientes:
+
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat" */
+
+function btnEncriptar(){
+    const textoEncriptado = encriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = '';
+    mensaje.style.backgroundImage = 'none'
+}
+
+function encriptar(stringEncriptada){
+    let matrizCodigo = [['e', 'enter'], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']];
+    stringEncriptada= stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringEncriptada.includes(matrizCodigo[i][0])){
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+    return stringEncriptada;
+}
+
+function btnDesencriptar(){
+    const textoEncriptado = desencriptar(textArea.value);
+    mensaje.value = textoEncriptado;
+    textArea.value = '';
+}
+
+function desencriptar(stringDesencriptada){
+    let matrizCodigo = [['e', 'enter'], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']];
+    stringDesencriptada= stringDesencriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++){
+        if(stringDesencriptada.includes(matrizCodigo[i][1])){
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0])
+        }
+    }
+    return stringDesencriptada;
+}
+
+function copiarTexto() {
+    const textoACopiar = mensaje.value; 
+
+    navigator.clipboard.writeText(textoACopiar)
+        .then(() => {
+            alert("Texto copiado al portapapeles");
+            mensaje.value = '';
+        })
+        .catch(err => {
+            console.error('Error al copiar el texto: ', err);
+
+        });
+}
